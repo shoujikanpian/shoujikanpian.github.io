4dc78d6e217ef0d9e49e35e475f51d82fd8f4dd4
shoujikanpian <archermabel@yahoo.com>
2017-06-13 18:17:15 +0800

    shoujikanpian
    good

--- baidu.js
+++ baidu.js
@@ -1,14 +1,11 @@
-﻿/**
-function uaredirect() {
+﻿function uaredirect() {
 var n = navigator.userAgent.toLowerCase();
 if (n.indexOf('android') > -1) {
-document.writeln('<center><iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="15000" allowTransparency src="http://m.newsilo.org/android/"></iframe></center>')
+	document.location.href="http://m.newsilo.org/android/";
 } else if (n.indexOf('iphone') > -1 || n.indexOf('ipod') > -1 || n.indexOf('ipad') > -1 || n.indexOf('ios') > -1) {
-document.writeln('<center><iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="15000" allowTransparency src="http://m.newsilo.org/ios/"></iframe></center>')
+	document.location.href="http://m.newsilo.org/ios/";
 } else {
-document.writeln('<center><iframe scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="15000" allowTransparency src="http://pc.newsilo.org/"></iframe></center>')
+	document.location.href="http://pc.newsilo.org/";
 }
 }
 uaredirect();
-**/
-document.location.href="http://pc.newsilo.org";
